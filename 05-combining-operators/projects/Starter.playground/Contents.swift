24dfb124b8131cd1e5f936076fac2affeb0e4954
Leviatanus <dominik.czerwoniuk@sieci.net.pl>
2023-04-05 11:12:14 +0200

    Chapter 5 - finish prepending and appending operators theory

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -3,7 +3,111 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+// Prepending - adding values before any publisher publishes anything
+
+example(of: "prepend(Output...)") {
+    // 1
+    let publisher = [3, 4].publisher
+    // 2
+    publisher
+        .prepend(1, 2)
+        .prepend(-1, 0) // The last prepend affects the upstream first
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "prepend(Sequence)") {
+    // 1
+    let publisher = [5, 6, 7].publisher
+    // 2
+    publisher
+        .prepend([3, 4])
+        .prepend(Set(1...2)) // sets are UNORDERED - the order is not guaranteed
+        .prepend(stride(from: 6, to: 11, by: 2))
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+// WARNING If we prepend the publisher, it must complete before we will switch to primary publisher
+
+example(of: "prepend(Publisher)") {
+    // 1
+    let publisher1 = [3, 4].publisher
+    let publisher2 = [1, 2].publisher
+    // 2
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher) #2") {
+    // 1
+    let publisher1 = [3, 4].publisher
+    let publisher2 = PassthroughSubject<Int, Never>()
+    // 2
+    publisher1
+        .prepend(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    // 3
+    publisher2.send(1)
+    publisher2.send(2)
+    publisher2.send(completion: .finished)
+}
+
+// Appending
+
+example(of: "append(Output...)") {
+    // 1
+    let publisher = [1].publisher
+    // 2
+    publisher
+        .append(2, 3)
+        .append(4)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "append(Output...) #2") {
+    // 1
+    let publisher = PassthroughSubject<Int, Never>()
+    publisher
+        .append(3, 4)
+        .append(5)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    // 2
+    publisher.send(1)
+    publisher.send(2)
+    publisher.send(completion: .finished) // no appending occurs unless the previous publisher sends a .finished completion event.
+}
+
+example(of: "append(Sequence)") {
+    // 1
+    let publisher = [1, 2, 3].publisher
+    publisher
+        .append([4, 5]) // 2
+        .append(Set([6, 7])) // 3
+        .append(stride(from: 8, to: 11, by: 2)) // 4
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+example(of: "append(Publisher)") {
+    // 1
+    let publisher1 = [1, 2].publisher
+    let publisher2 = [3, 4].publisher
+    // 2
+    publisher1
+        .append(publisher2)
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+// Advanced combining
+
+
 
 // Copyright (c) 2021 Razeware LLC
 //
