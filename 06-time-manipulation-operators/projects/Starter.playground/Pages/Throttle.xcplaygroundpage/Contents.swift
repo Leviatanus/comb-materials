2b995fa51337d62816e595ab32ae48fc7f99ce67
Leviatanus <dominik.czerwoniuk@sieci.net.pl>
2023-04-06 10:47:10 +0200

    Finish timeout example

--- 06-time-manipulation-operators/projects/Starter.playground/Pages/Throttle.xcplaygroundpage/Contents.swift
+++ 06-time-manipulation-operators/projects/Starter.playground/Pages/Throttle.xcplaygroundpage/Contents.swift
@@ -2,8 +2,42 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add your code here #>
+let throttleDelay = 1.0
 
+let subject = PassthroughSubject<String, Never>()
+// 2
+let throttled = subject
+    .throttle(for: .seconds(throttleDelay), scheduler:
+                DispatchQueue.main, latest: true)
+// 3
+    .share() // Like in debounce example, adding the share() operator here guarantees that all subscribers see the same output at the same time from the throttled subject.
+
+let subjectTimeline = TimelineView(title: "Emitted values")
+let throttledTimeline = TimelineView(title: "Throttled values")
+
+let view = VStack(spacing: 100) {
+    subjectTimeline
+    throttledTimeline
+}
+PlaygroundPage.current.liveView = UIHostingController(rootView:
+                                                        view.frame(width: 375, height: 600))
+subject.displayEvents(in: subjectTimeline)
+throttled.displayEvents(in: throttledTimeline)
+
+let subscription1 = subject
+    .sink { string in
+        print("+\(deltaTime)s: Subject emitted: \(string)")
+    }
+let subscription2 = throttled
+    .sink { string in
+        print("+\(deltaTime)s: Throttled emitted: \(string)")
+    }
+
+subject.feed(with: typingHelloWorld)
+
+/// `debounce` vs `throttle`
+/// `debounce` waits for a pause in values it receives, then emits the latest one after the specified interval.
+/// `throttle` waits for the specified interval, then emits either the first or the latest of the values it received during that interval. It doesn’t care about pauses.
 //: [Next](@next)
 /*:
  Copyright (c) 2021 Razeware LLC
--- 06-time-manipulation-operators/projects/Starter.playground/Pages/Timeout.xcplaygroundpage/Contents.swift
+++ 06-time-manipulation-operators/projects/Starter.playground/Pages/Timeout.xcplaygroundpage/Contents.swift
@@ -2,7 +2,27 @@ import Combine
 import SwiftUI
 import PlaygroundSupport
 
-<# Add your code here #>
+enum TimeoutError: Error {
+  case timedOut
+}
+
+let subject = PassthroughSubject<Void, TimeoutError>()
+// Have you noticed you’re using a subject that emits Void values? Yes, this is totally legitimate! It signals that something happened. But, there is no particular value to carry. So, you simply use Void as the value type. This is such a common case that Subject has an extension with a send() function that takes no parameter in case the Output type is Void. This saves you from writing the awkward subject.send(()) statement!
+
+// if we did not add customError: (() -> Self.Failure)? the publisher would emit normal completion after given timeout
+let timedOutSubject = subject.timeout(.seconds(5), scheduler: DispatchQueue.main) {
+    .timedOut
+}
+
+let timeline = TimelineView(title: "Button taps")
+let view = VStack(spacing: 100) {
+    // 1
+    Button(action: { subject.send() }) {
+        Text("Press me within 5 seconds")
+    }
+    timeline }
+PlaygroundPage.current.liveView = UIHostingController(rootView: view.frame(width: 375, height: 600))
+timedOutSubject.displayEvents(in: timeline)
 
 //: [Next](@next)
 /*:
