7d8d734cae177ed92c320a826c093e0d8039750a
Leviatanus <dominik.czerwoniuk@sieci.net.pl>
2023-04-10 15:14:25 +0200

    FINISHED CHAPTER 14 - In Practice: Project "News", skip UIKIt challenge

--- 14-in-practice-project-news/projects/starter/API.playground/Contents.swift
+++ 14-in-practice-project-news/projects/starter/API.playground/Contents.swift
@@ -3,48 +3,120 @@ import PlaygroundSupport
 import Combine
 
 struct API {
-  /// API Errors.
-  enum Error: LocalizedError {
-    case addressUnreachable(URL)
-    case invalidResponse
-    
-    var errorDescription: String? {
-      switch self {
-      case .invalidResponse: return "The server responded with garbage."
-      case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
-      }
+    /// API Errors.
+    enum Error: LocalizedError {
+        case addressUnreachable(URL)
+        case invalidResponse
+        
+        var errorDescription: String? {
+            switch self {
+            case .invalidResponse: return "The server responded with garbage."
+            case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
+            }
+        }
     }
-  }
-  
-  /// API endpoints.
-  enum EndPoint {
-    static let baseURL = URL(string: "https://hacker-news.firebaseio.com/v0/")!
     
-    case stories
-    case story(Int)
-    
-    var url: URL {
-      switch self {
-      case .stories:
-        return EndPoint.baseURL.appendingPathComponent("newstories.json")
-      case .story(let id):
-        return EndPoint.baseURL.appendingPathComponent("item/\(id).json")
-      }
+    /// API endpoints.
+    enum EndPoint {
+        static let baseURL = URL(string: "https://hacker-news.firebaseio.com/v0/")!
+        
+        case stories
+        case story(Int)
+        
+        var url: URL {
+            switch self {
+            case .stories:
+                return EndPoint.baseURL.appendingPathComponent("newstories.json")
+            case .story(let id):
+                return EndPoint.baseURL.appendingPathComponent("item/\(id).json")
+            }
+        }
     }
-  }
-
-  /// Maximum number of stories to fetch (reduce for lower API strain during development).
-  var maxStories = 10
-
-  /// A shared JSON decoder to use in calls.
-  private let decoder = JSONDecoder()
-  
-  // <#Add your API code here#>
-  
+    
+    /// Maximum number of stories to fetch (reduce for lower API strain during development).
+    var maxStories = 10
+    
+    // <#Add your API code here#>
+    
+    /// A shared JSON decoder to use in calls.
+    private let decoder = JSONDecoder()
+    
+    // create a background thread for handling the requests
+    private let apiQueue = DispatchQueue(label: "API",
+                                         qos: .default,
+                                         attributes: .concurrent)
+    
+    func story(id: Int) -> AnyPublisher<Story, Error> {
+        URLSession.shared
+            .dataTaskPublisher(for: EndPoint.story(id).url)
+            .receive(on: apiQueue) // using the custom background thread
+            .map(\.data)
+            .decode(type: Story.self, decoder: decoder)
+            .catch { _ in Empty<Story, Error>() } // if error happens, we use Empty which is a publisher that completes immediately without emitting any output values
+            .eraseToAnyPublisher()
+    }
+    
+    // custom implementation of the MergeMany publisher. We merge many publshers from story(id:) into one
+    func mergedStories(ids storyIDs: [Int]) -> AnyPublisher<Story, Error> {
+        let storyIDs = Array(storyIDs.prefix(maxStories))
+        
+        precondition(!storyIDs.isEmpty)
+        
+        let initialPublisher = story(id: storyIDs[0])
+        let remainder = Array(storyIDs.dropFirst())
+        
+        return remainder
+            .reduce(initialPublisher) { combined, id in
+                return combined
+                    .merge(with: story(id: id))
+                    .eraseToAnyPublisher()
+            }
+    }
+    
+    func stories() -> AnyPublisher<[Story], Error> {
+        URLSession.shared
+            .dataTaskPublisher(for: EndPoint.stories.url) // publisher for given URL session data task
+            .map(\.data) // get only data part, ditch the response
+            .decode(type: [Int].self, decoder: decoder) // decode the response to an array of Ints (we get stories ids)
+            .mapError { error -> API.Error in
+                switch error {
+                case is URLError:
+                    return Error.addressUnreachable(EndPoint.stories.url)
+                default:
+                    return Error.invalidResponse
+                }
+            } // handle errors
+            .filter { !$0.isEmpty } // ditch empty result
+            .flatMap { storyIDs in
+                return self.mergedStories(ids: storyIDs)
+            } // returned merged publishers
+            .scan([]) { stories, story -> [Story] in
+              return stories + [story]
+            } // for each emitted value accumulate the collected stories into growing array and emit that array
+            .map { $0.sorted() } // sort the array
+            .eraseToAnyPublisher()
+    }
+    
 }
 
 // <#Call the API here#>
+let api = API()
+var subscriptions = [AnyCancellable]()
 
+//api.story(id: 1000)
+//    .sink(receiveCompletion: { print($0) },
+//          receiveValue: { print($0) })
+//    .store(in: &subscriptions)
+
+//api.mergedStories(ids: [1000, 1001, 1002])
+//    .sink(receiveCompletion: { print($0) },
+//          receiveValue: { print($0) })
+//    .store(in: &subscriptions)
+
+api.stories()
+   .sink(receiveCompletion: { print($0) },
+         receiveValue: { print($0) })
+   .store(in: &subscriptions)
 
 // Run indefinitely.
 PlaygroundPage.current.needsIndefiniteExecution = true
